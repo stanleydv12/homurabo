9dcd30bffd4ca40a327e3b210470ee72e2a50b24
stanleydv12 <sdaveteherag@gmail.com>
2024-10-10 09:12:19 +0700

    fix(script): change disk only to default

--- scripts/create-vm-snapshot/create-vm-snapshot.sh
+++ scripts/create-vm-snapshot/create-vm-snapshot.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Directory to store snapshots
-SNAPSHOT_DIR="/var/lib/libvirt/snapshots"
-# Current date
-DATE=$(date +%Y-%m-%d)
-# Current Snapshot directory
-BASE_SNAPSHOT_DIR="$SNAPSHOT_DIR/$DATE"
-mkdir -p "$BASE_SNAPSHOT_DIR"
+# Get current date
+date=$(date +"%d-%m-%Y")
 
 # List all VMs
 vms=$(virsh list --all --name)
@@ -14,16 +9,11 @@ vms=$(virsh list --all --name)
 # Create snapshot for each VM
 for vm in $vms; do
     if [ -n "$vm" ]; then
-        echo "Creating disk-only snapshot for $vm VM"
+        echo "Creating snapshot for $vm VM"
 
-         # Define the snapshot file path
-        snapshot_file="${BASE_SNAPSHOT_DIR}/${vm}_snapshot.qcow2"
+        name="${vm}-snapshot-${date}"
 
-        # Create the disk-only snapshot with the specified file path
-        virsh snapshot-create-as --domain "$vm" --name "${vm}_snapshot" \
-          --disk-only --diskspec vda,file="$snapshot_file" --atomic
-
-        echo "Snapshot created: $snapshot_file"
-    fi
+        # Create the snapshot with the specified file path
+        virsh snapshot-create-as --domain "$vm" --name "$name"
+    fi 
 done
-
